1d41b9c5a0a39f6937beb41263d7465c50e65744
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-22 10:03:42 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Faça um programa que estime o custo de consumo de energia de um aparelho elétrico dado o seu consumo em kWh/mês e o uso diário (em horas).
+
+Console.WriteLine("--- Custo de Energia ---\n");
+
+Console.Write("Qual é o consumo mensal do aparelho em KWh? ");
+double CustoEnergia = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Quantas horas p/dia o aparelho é usado? ");
+double horasDiarias = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Qual é o custo do KWh em reais? ");
+double custoKwh = Convert.ToDouble(Console.ReadLine());
+
+double custoMensal = horasDiarias * CustoEnergia * custoKwh;
+
+Console.WriteLine($"\nO custo de um mês de uso é: R$ {custoMensal}");
+
